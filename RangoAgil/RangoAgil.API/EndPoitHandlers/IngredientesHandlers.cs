979e0858dac877f58f87b9ff45db5475b0d90c9d
Ivan Clay <ivan.clay.moura@gmail.com>
2025-03-02 11:56:29 -0300

    added endpoints handlers

---
+++ RangoAgil/RangoAgil.API/EndPoitHandlers/IngredientesHandlers.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using RangoAgil.API.DbContexts;
+using RangoAgil.API.Models;
+
+namespace RangoAgil.API.EndPoitHandlers;
+
+public static class IngredientesHandlers
+{
+    public static async Task<IEnumerable<IngredienteDTO>> GetIngredientesByRangoId
+    (RangoDbContext context,
+     IMapper mapper,
+    int rangoId) 
+{
+    return mapper.Map<IEnumerable<IngredienteDTO>>((await context
+                    .Rangos
+                    .Include(rango => rango.Ingredientes)
+                    .FirstOrDefaultAsync(rango => rango.Id == rangoId))?.Ingredientes);
+}
+}
